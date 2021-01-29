0ba06746d11314290bd1ea36058df7f13a26dc44
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-29 15:13:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,15 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
+  final_results = data.each_with_objective({}) do |(key, value), final_array|
+    
   # write your code here!
-  binding.pry
+  # binding.pry
+  value.each do |inner_key, names|
+    name.each do |name|
+    end
+  end 
+  final_array
+end
 
 end
