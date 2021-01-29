a08ed5250ef0fff895699158e2e3f8d37f13a618
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-29 15:40:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,18 +5,14 @@ new_hash = {}
 data.each do |key, value|
   value.each do |new_values, names|
   names.each do |name|
-    if !new_hash[name]
-      new_hash[name] = {}
-    end
-    if !new_hash[names][key]
-      new_hash[name][key] = []
-    end
-    new_hash[name][key] << new_values.to_s
+  new_hash [name] || {}
+  new_hash[name][key] || = []
+  new_hash[name][key] << new_values.to_s
   end
 end
 end
 
-
+new_hash
   # write your code here!
 
 end
