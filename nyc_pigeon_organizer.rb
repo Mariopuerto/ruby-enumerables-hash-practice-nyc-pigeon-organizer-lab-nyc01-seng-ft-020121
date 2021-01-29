0bc0963cdea8d0a8b1113c188b7f86c0322a92d8
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-29 15:32:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,22 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  final_results = data.each_with_objective({}) do |(key, value), final_array|
-
+new_hash = {} 
+data.each do |key, value| 
+  value.each do |new_values, names|
+  names.each do |name|
+    if !new_hash[name]
+      new_hash[name] = {}
+    end 
+    if !new_hash[names][key]
+      new_hash[name][key] = []
+    end 
+    new_hash[name][key] << new_values.to_s
+  end 
+end 
+end 
+new_hash
+    
   # write your code here!
-  # binding.pry
-  value.each do |inner_key, names|
-    name.each do |name|
-    end
-  end
-  final_array
-end
 
 end
